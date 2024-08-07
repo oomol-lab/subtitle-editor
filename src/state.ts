65af9f6b498a645c661e27b55d9f56589f8fe1ed
TaoZeyu <i@taozeyu.com>
2024-08-07 09:04:23 +0800

    feat: optimize selection logic & view

--- src/state.ts
+++ src/state.ts
@@ -1,11 +1,14 @@
 import { Val } from "value-enhancer";
-import { BaseSetSelectionOperation, Editor, Operation } from "slate";
+import { Point, Location, Range, Editor, Operation, Span, SelectionMode, SetSelectionOperation } from "slate";
 import { Leaf } from "./data";
 
 export class State {
 
     readonly #editor: Editor;
+
+    #lastAnchor: Point | null = null;
     #lastSelected$: Val<boolean> | null = null;
+    #lastSelectedNode: Leaf | null = null;
 
     public constructor(editor: Editor) {
         const protoApply = editor.apply;
@@ -23,30 +26,92 @@ export class State {
         }
     }
 
-    #onSelectionChange(operation: BaseSetSelectionOperation): void {
-        let didChangeSuccess = false;
-        const anchor = operation.newProperties?.anchor;
-        if (anchor) {
-            const result = this.#editor.nodes({
-                at: anchor,
-            });
-            let node: Leaf | undefined
-            for (const [_node, _path] of result) {
-                node = _node as Leaf;
-            }
-            if ((node as any)?.selected$) {
-                const selected$: Val<boolean> = (node as any).selected$;
-                if (this.#lastSelected$ !== selected$) {
-                    selected$.set(true);
-                    this.#lastSelected$?.set(false);
-                    this.#lastSelected$ = selected$;
-                    didChangeSuccess = true;
+    #onSelectionChange(operation: SetSelectionOperation): void {
+        let selectNothing = true;
+        if (operation.newProperties) {
+            const [begin, end] = this.#updateAndGetPoints(operation.newProperties as Range);
+            let chooseNode: Leaf | null = null;
+
+            if (Point.equals(begin, end)) {
+                const node = this.#searchNode(begin);
+                chooseNode = node || null;
+                selectNothing = false;
+
+                if (chooseNode && begin.offset === 0 && begin.path[1] !== 0) {
+                    // offset 为 0 时，实际上打字会插入上一个节点。
+                    // 调整行为，让高亮选择的节点永远是打字输入的节点。
+                    const beforePath = this.#editor.before(begin);
+                    if (beforePath) {
+                        const beforeNode = this.#searchNode(beforePath!, "lowest");
+                        if (beforeNode) {
+                            chooseNode = beforeNode;
+                        }
+                    }
+                }
+            } else {
+                const nodeBegin = this.#searchNode(begin, "lowest");
+                const nodeEnd = this.#searchNode(end, "lowest");
+                const selectedBegin$ = (nodeBegin as any)?.selected$;
+                const selectedEnd$ = (nodeEnd as any)?.selected$;
+                if (selectedBegin$ && selectedEnd$ && selectedBegin$ === selectedEnd$) {
+                    chooseNode = nodeBegin || null;
+                    selectNothing = false;
                 }
             }
+            if (chooseNode) {
+                this.#chooseSelectedNode(chooseNode);
+            }
         }
-        if (this.#lastSelected$ && !didChangeSuccess) {
+        if (selectNothing) {
+            this.#chooseSelectedNode(null);
+        }
+    }
+
+    #chooseSelectedNode(node: Leaf | null): void {
+        if (node) {
+            const selected$ = (node as any).selected$;
+            if (!selected$) {
+                throw new Error("node is invalid leaf");
+            }
+            if (selected$ !== this.#lastSelected$) {
+                selected$.set(true);
+                this.#lastSelected$?.set(false);
+                this.#lastSelected$ = selected$;
+                this.#lastSelectedNode = node;
+            }
+        } else if (this.#lastSelected$) {
             this.#lastSelected$.set(false);
             this.#lastSelected$ = null;
+            this.#lastSelectedNode = null;
         }
+    }
+
+    #updateAndGetPoints({ anchor, focus }: Range): [Point, Point] {
+        if (anchor) {
+            this.#lastAnchor = anchor;
+        } else if (this.#lastAnchor) {
+            anchor = this.#lastAnchor;
+        }
+        if (!anchor && !focus) {
+            throw new Error("invalid state");
+        }
+        if (!anchor || !focus) {
+            const point = anchor || focus;
+            return [point, point];
+        }
+        if (Point.isBefore(anchor, focus)) {
+            return [anchor, focus];
+        } else {
+            return [focus, anchor];
+        }
+    }
+
+    #searchNode(at: Location | Span, mode?: SelectionMode): Leaf | undefined {
+        const result = this.#editor.nodes({ at, mode });
+        let node: Leaf | undefined;
+        for (const [_node, _path] of result) {
+            node = _node as Leaf;
+        }
+        return node;
     }
 }
