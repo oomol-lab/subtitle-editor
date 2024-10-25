ddaaffa9d586ed6fe4de6273dfe54d5f6f7907ac
CheerChen <meetcheerego@gmail.com>
2024-10-25 15:20:25 +0800

    chore(project): optimize the eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,32 @@
 import antfu from "@antfu/eslint-config";
 
 export default antfu({
-    // Or customize the stylistic rules
-    stylistic: {
-        indent: 4,
-        semi: true,
-        quotes: "double",
+  // Or customize the stylistic rules
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+
+  // TypeScript and Vue are autodetected, you can also explicitly enable them:
+  typescript: {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
+  },
+  react: true,
+  yaml: false,
+  markdown: false,
+  jsonc: false,
 
-    // TypeScript and Vue are autodetected, you can also explicitly enable them:
-    typescript: {
-        languageOptions: {
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
-    },
-    react: true,
-    yaml: false,
-    markdown: false,
-    jsonc: false,
+  lessOpinionated: true,
 
-    lessOpinionated: true,
-
-    // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
-    ignores: [
-        "**/fixtures",
-        "patches/**",
-    ],
+  // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
+  ignores: [
+    "**/fixtures",
+    "patches/**",
+  ],
 });
