3ffd49a6ea9af1c2e51f9a48fde5c8beedd61ded
CheerChen <meetcheerego@gmail.com>
2024-10-26 16:31:32 +0800

    feat(pages): use the theme provider components

--- desktop/renderer/src/App.tsx
+++ desktop/renderer/src/App.tsx
@@ -1,6 +1,8 @@
 import type { PropsWithChildren } from "react";
 import React from "react";
+import { useVal } from "use-value-enhancer";
 import { AppContextProvider } from "./components/AppContextProvider";
+import { ThemeProvider } from "./components/ThemeProvider";
 import { type AppContext, Routes } from "./routes";
 
 export interface StudioHomeProps {
@@ -11,10 +13,16 @@ export const StudioHome = ({
   appContext,
   children,
 }: PropsWithChildren<StudioHomeProps>) => {
+  const prefersColorScheme = useVal(appContext.settingStore.prefersColorScheme$);
+
   return (
     <AppContextProvider context={appContext}>
-      <Routes />
-      {children}
+      <ThemeProvider
+        prefersColorScheme={prefersColorScheme}
+      >
+        <Routes />
+        {children}
+      </ThemeProvider>
     </AppContextProvider>
   );
 };
--- desktop/renderer/src/index.tsx
+++ desktop/renderer/src/index.tsx
@@ -1,10 +1,20 @@
 import type { AppContext } from "./routes";
 import React from "react";
-
 import { createRoot } from "react-dom/client";
-import { StudioHome } from "./App";
 
-const appContext: AppContext = {};
+import { val } from "value-enhancer";
+import { StudioHome } from "./App";
+import { OS } from "./routes/constants";
+import { SettingStore } from "./stores/setting.store";
+import "./style.css";
+
+const os$ = val(OS.Windows);
+const settingStore = new SettingStore();
+
+const appContext: AppContext = {
+  os$,
+  settingStore,
+};
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
