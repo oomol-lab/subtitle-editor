8b6c14c98a2b29432df53ab11c93dd312af8928e
CheerChen <meetcheerego@gmail.com>
2024-10-27 10:04:36 +0800

    feat(pages): use translate hooks to setting page

--- desktop/renderer/src/components/AppearancePicker/index.tsx
+++ desktop/renderer/src/components/AppearancePicker/index.tsx
@@ -4,8 +4,9 @@ import type { OOMOLPrefersColorScheme } from "../ThemeProvider";
 import { Radio } from "antd";
 
 import React from "react";
-import styles from "./AppearancePicker.module.scss";
+import { useTranslate } from "val-i18n-react";
 
+import styles from "./AppearancePicker.module.scss";
 import autoSVG from "./icons/auto.svg";
 import darkSVG from "./icons/dark.svg";
 import lightSVG from "./icons/light.svg";
@@ -19,29 +20,26 @@ export const AppearancePicker: React.FC<AppearancePickerProps> = ({
   defaultValue,
   changeAppearance,
 }) => {
-  // const t = useTranslate();
+  const t = useTranslate();
   return (
     <div className={styles.container}>
       <Radio.Group defaultValue={defaultValue} onChange={changeAppearance}>
         <Radio value="light">
           <div className={styles.options}>
             <img src={lightSVG} />
-            {/* <span>{t("settings.theme-light")}</span> */}
-            <span>light</span>
+            <span>{t("settings.theme-light")}</span>
           </div>
         </Radio>
         <Radio value="dark">
           <div className={styles.options}>
             <img src={darkSVG} />
-            {/* <span>{t("settings.theme-dark")}</span> */}
-            <span>dark</span>
+            <span>{t("settings.theme-dark")}</span>
           </div>
         </Radio>
         <Radio value="auto">
           <div className={styles.options}>
             <img src={autoSVG} />
-            {/* <span>{t("settings.theme-auto")}</span> */}
-            <span>auto</span>
+            <span>{t("settings.theme-auto")}</span>
           </div>
         </Radio>
       </Radio.Group>
